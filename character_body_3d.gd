d7ba71ba60611716a4c4bd5d8d42abf572e25057
pokemaz <98837743+MA-Briggs@users.noreply.github.com>
2024-08-16 23:02:34 +0100

    grabbing works but needs cleaned

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -3,18 +3,27 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const RAY_LENGTH = 1000
+
 
 var mouse_sensitivity = 0.002  # radians/pixel
 
 
-@onready var camera = $PlayerCamera
-@onready var ray = $RayCast3D
+@onready var camera = $Pivot/PlayerCamera
+var carrying :RigidBody3D = null
+#var joint
 
 func _init():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
-	# Add the gravity.
+	
+	var space_state = get_world_3d().direct_space_state
+	var mousepos = get_viewport().get_mouse_position()
+	
+	
+
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -22,7 +31,36 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("right_click") && carrying == null:
+		var origin = camera.project_ray_origin(mousepos)
+		var end = origin + camera.project_ray_normal(mousepos) * RAY_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+		var result = space_state.intersect_ray(query)
+		
+		var the_node := result['collider'] as RigidBody3D
+		if the_node.is_in_group('grabable'):
+			carrying = the_node
+			
+	elif Input.is_action_just_pressed("right_click") && carrying:
+		carrying.freeze = false
+		var carry_col := carrying.get_child(0) as CollisionShape3D
+		carry_col.disabled = false
+		carrying = null
+		
+			
+	if carrying:
+		
+		carrying.freeze = true
+		var carry_col := carrying.get_child(0) as CollisionShape3D
+		carry_col.disabled = true
+		
+		carrying.position = $Pivot/Hands.global_position
+		carrying.rotation = $Pivot/Hands.global_rotation
 
+
+		
+		 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
