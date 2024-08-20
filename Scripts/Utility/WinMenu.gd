29136ea06c5e21aaa5ee7ad1fe1525c3754c80a7
pokii-hub <2100571@uad.ac.uk>
2024-08-20 16:12:56 +0100

    menu implemented

---
+++ Scripts/Utility/WinMenu.gd
@@ -0,0 +1,61 @@
+extends CanvasLayer
+
+@onready var game_scene
+@onready var start_scene
+
+var hover
+var click
+
+func _ready() -> void:
+	hover = preload("res://Assets/SFX/button_hover.wav")
+	click = preload("res://Assets/SFX/button_click.wav")
+	MusicPlayer._play_song("start_menu")
+	if MusicPlayer.volume_db <= -10:
+		MusicPlayer._transition_in()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	game_scene = load("res://Scenes/main_world.tscn")
+	start_scene = load("res://Scenes/Menu/start_menu.tscn")
+
+
+
+func _on_retry_pressed() -> void:
+	_play_sound("click")
+	await $AudioStreamPlayer.finished
+	MusicPlayer._transition_out()
+	await MusicPlayer.fading_out == false
+	get_tree().change_scene_to_packed(game_scene)
+
+
+func _on_menu_pressed() -> void:
+	_play_sound("click")
+	await $AudioStreamPlayer.finished
+	MusicPlayer._transition_out()
+	await MusicPlayer.fading_out == false
+	get_tree().change_scene_to_packed(start_scene)
+
+
+func _on_quit_pressed() -> void:
+	_play_sound("click")
+	MusicPlayer._transition_out()
+	await MusicPlayer.fading_out == false
+	get_tree().quit()
+
+
+func _on_retry_mouse_entered() -> void:
+	_play_sound("hover")
+
+
+func _on_menu_mouse_entered() -> void:
+	_play_sound("hover")
+
+
+func _on_quit_mouse_entered() -> void:
+	_play_sound("hover")
+
+func _play_sound(name:String):
+	match(name):
+		"hover":
+			$AudioStreamPlayer.stream = hover
+		"click":
+			$AudioStreamPlayer.stream = click
+	$AudioStreamPlayer.play()
