ae55dccf255a163996d43be26587195fea981977
pokemaz <98837743+MA-Briggs@users.noreply.github.com>
2024-08-20 14:39:33 +0100

    finished winzone

--- win_zone.gd
+++ win_zone.gd
@@ -1,8 +1,10 @@
 extends Area3D
 class_name WinZone
 
+var start_scene
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start_scene = preload("res://Scenes/Menu/start_menu.tscn")
 	pass # Replace with function body.
 
 
@@ -16,6 +18,7 @@ func _on_body_entered(body):
 	if Plate:
 		if Plate.is_pie:
 			#,make scene change here
+			get_tree().change_scene_to_packed(start_scene)
 			pass
 		
 	pass # Replace with function body.
